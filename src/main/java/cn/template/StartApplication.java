a56dd7334c3c887a9a70af35f5f59b76ea14a6ac
licq <18384906534@163.com>
2022-07-12 11:43:38 +0800

    '初始化'

---
+++ src/main/java/cn/template/StartApplication.java
@@ -0,0 +1,16 @@
+package cn.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author licq
+ * @date 2022/7/12
+ */
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
